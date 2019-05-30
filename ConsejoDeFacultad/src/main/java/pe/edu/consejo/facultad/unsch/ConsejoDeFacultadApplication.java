07bb1a2b39aef14725f639ad1a1e490c28a707af
WilderPariona <jhon.pariona.27@unsch.edu.pe>
2019-05-29 22:00:14 -0500

    PRIMER COMMIT DEL PROYECTO

---
+++ ConsejoDeFacultad/src/main/java/pe/edu/consejo/facultad/unsch/ConsejoDeFacultadApplication.java
@@ -0,0 +1,13 @@
+package pe.edu.consejo.facultad.unsch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConsejoDeFacultadApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConsejoDeFacultadApplication.class, args);
+	}
+
+}
